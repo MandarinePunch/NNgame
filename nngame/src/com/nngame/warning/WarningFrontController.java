ba2649586e0ff975dfd8c61592bf6d8c10b8b87e
MandarinePunch <93591074+MandarinePunch@users.noreply.github.com>
2022-06-22 11:47:17 +0900

    고객지원 writeok 파일추가

---
+++ nngame/src/com/nngame/warning/WarningFrontController.java
@@ -0,0 +1,52 @@
+package com.nngame.warning;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.nngame.action.ActionForward;
+
+@WebServlet(name = "WarningFrontController", urlPatterns = "/warning/*")
+public class WarningFrontController extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+	
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String requestURI = request.getRequestURI();
+		ActionForward forward = null;
+		
+		if(requestURI.equals("/warning/approach")) {
+			forward = new ActionForward(false, "/warning_jsp/preventURL.jsp");
+		} 
+		
+		if(forward != null) {			
+			if(forward.isRedirect()) {
+				response.sendRedirect(forward.getPath());
+			} else {
+				RequestDispatcher dis = request.getRequestDispatcher(forward.getPath());
+				dis.forward(request, response);
+			}
+		}
+	}
+}
+
+
+
+
+
+
+
+
